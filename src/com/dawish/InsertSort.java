f893699d995fb5a527a54e80a08d4e14ea7b4f24
但行希 <danxx@wtvdev.com>
2016-02-15 20:19:05 +0800

    #插入排序添加注释

--- src/com/dawish/InsertSort.java
+++ src/com/dawish/InsertSort.java
@@ -20,18 +20,26 @@ public class InsertSort {
 	static int[] sort(int[] arr){
 		
 		 int outer, inner;
-		 for (outer = 1; outer < arr.length; outer++){
+		 /*
+		  * 第一个作为本来就有的数据，第二个作为插入的数据
+		  */
+		 for (outer = 1; outer < arr.length; outer++){ 
 		        //current sort value
 		        int temp = arr[outer];
 		        inner = outer;
-		        //move range right
-		        while (inner > 0 && arr[inner - 1] >= temp)
+		        //move range right 
+		        while (inner > 0 && arr[inner - 1] >= temp)  //前面的大就想后面移动
 		        {
 		            arr[inner] = arr[inner - 1];
 		            --inner;
 		        }
 		        arr[inner] = temp;
-
+		        
+		        System.out.print("第"+outer+"次循环后的结果:");
+		        for(int i:arr){
+					System.out.print(i+"   ");
+				}
+		        System.out.println("");
 		    }
 		
 		return arr;
